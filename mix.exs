42b8c2490a924d77728bb6a9d68d53679cd85f92
Aleksei Magusev <lexmag@me.com>
2014-09-11 23:14:08 +0200

    Bump Elixir requirement
    Yayfor v1.0.0

--- mix.exs
+++ mix.exs
@@ -4,28 +4,11 @@ defmodule Tty2048.Mixfile do
   def project do
     [app: :tty2048,
      version: "0.0.1",
-     elixir: "~> 0.15.1",
-     deps: deps]
+     elixir: "~> 1.0.0"]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [],
      mod: {Tty2048, []}]
-  end
-
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
-  defp deps do
-    []
   end
 end
